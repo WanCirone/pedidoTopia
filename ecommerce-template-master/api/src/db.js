9d1875b8dace51e75bec8187461357a32d281a72
GerMoren <ingenieriamg91@gmail.com>
2020-09-22 13:44:48 -0300

    sdk meli

--- ecommerce-template-master/api/src/db.js
+++ ecommerce-template-master/api/src/db.js
@@ -37,7 +37,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Product, Category, Orders } = sequelize.models;
+const { Product, Category, Orders, Prviders } = sequelize.models;
 
 // Aca vendrian las relaciones
 Product.hasMany(Category);

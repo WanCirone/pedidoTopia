c9e96b3126181ac6790a42fc8b735d79cd197b06
GerMoren <ingenieriamg91@gmail.com>
2020-09-22 11:07:34 -0300

    primer commit

--- ecommerce-template-master/api/src/routes/index.js
+++ ecommerce-template-master/api/src/routes/index.js
@@ -1,13 +1,11 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // import all routers;
-const productRouter = require('./product.js');
-
+const productRouter = require("./product.js");
 
 const router = Router();
-
 // load each router on a route
 // i.e: router.use('/auth', authRouter);
 // router.use('/auth', authRouter);
-router.use('/products', productRouter);
+router.use("/products", productRouter);
 
 module.exports = router;

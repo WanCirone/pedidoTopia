be72222c7ed7bd6cc3dfdf9fd5d56f01d21f826c
joaquinquiroga22 <joaquinquiroga22@gmail.com>
2020-09-23 14:50:02 -0300

    hola

--- ecommerce-template-master/client/src/App.js
+++ ecommerce-template-master/client/src/App.js
@@ -3,30 +3,23 @@ import './App.css';
 import styles from "./App.css";
 import { BrowserRouter as Router, Route , Switch} from 'react-router-dom'
 import Button from "@material-ui/core/Button";
-import Footer from "./Footer/Footer.js";
+// import Footer from "./Footer/Footer.js";
 import Add_Product from "./AddProduct/Add_Product.js"
+import Navbar from './Navbar/Navbar.js';
+import Table_Products from "./Table_Products/Table_Products.jsx"
 function App() {
   return (
     <Router>
          <div className={styles.App}>
-           <h1>
-             PedidoTopia
-           </h1>
-           <div style={{ marginLeft: "978px" }}>
-           <Button>
-             Listado de Productos
-           </Button>
-         </div>
-         <hr />
          <Switch>
          <Route path = "/product" exact>
            <Add_Product/>
          </Route>
-         {/* <Route path = "/table" exact>
-           <TableProduct/>
-         </Route> */}
-         <Route path = "/" exact>
-           <Footer/>
+          <Route path = "/table" exact>
+           <Table_Products/>
+         </Route> 
+         <Route path = "/">
+           <Navbar/>
          </Route>
          </Switch>
          </div>
